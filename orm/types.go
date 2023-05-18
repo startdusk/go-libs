afedb7dae411d455bb8d7a5f58b0995eb705f04f
startdusk <522383345@qq.com>
2023-05-18 21:51:47 +0800

    add orm interface define

---
+++ orm/types.go
@@ -0,0 +1,33 @@
+package orm
+
+import (
+	"context"
+	"database/sql"
+)
+
+// Querier 用于 `SELECT` 语句
+type Querier[T any] interface {
+	Get(ctx context.Context) (*T, error)
+	GetMulti(ctx context.Context) ([]*T, error)
+
+
+	// 返回值的形式也可以, 但一般返回指针
+	// 返回指针 是允许在 AOP 的场景下修改返回值, 从而不引起数据拷贝
+	// 虽然返回指针在一些场景下会有内存逃逸的问题, 但这是中间件开发, 没到那种极致性能的优化, 开发速度是第一优先
+	// Get(ctx context.Context) (*T, error)
+	// GetMulti(ctx context.Context) (*T, error)
+}
+
+// Executor 用于 `INSERT`, `UPDATE`, `DELETE` 语句
+type Executor interface {
+	Exec(ctx context.Context) (sql.Result, error)
+}
+
+type QueryBuilder interface {
+	Build() (*Query, error)
+}
+
+type Query struct {
+	SQL string
+	Args []any
+}
